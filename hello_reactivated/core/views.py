f0e677fcad9aaa207ee3ae9db6318051a2c0dae2
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2024-03-26 23:02:41 -0700

    Add route decorator for URL routing

--- hello_reactivated/core/views.py
+++ hello_reactivated/core/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from hello_reactivated.route_decorator import Route
+
 from . import templates
 
 
+route = Route()
+
+@route("/")
 def index(request):
     return templates.Default(name="World").render(request)
