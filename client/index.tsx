b1c5382839e319eafa8001d3e70a1fece148494e
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2024-03-26 23:47:34 -0700

    Add favicon and meta tags to index.tsx

--- client/index.tsx
+++ client/index.tsx
@@ -2,15 +2,26 @@ import React from "react";
 import { hydrate } from "react-dom";
 
 import { Provider, getServerData, getTemplate } from "@reactivated";
-import { HelmetProvider } from "react-helmet-async";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 
 const { props, context } = getServerData();
 
 const Template = await getTemplate(context);
 
+
 hydrate(
   <HelmetProvider>
     <Provider value={context}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Hello Reactivated</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          href={`${context.STATIC_URL}favicon.ico`}
+          type="image/x-icon"
+        />
+      </Helmet>
       <Template {...props} />
     </Provider>
   </HelmetProvider>,
